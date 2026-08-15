6aa928b43d7e1c116a50914018a590dfc1e24aaa
Mahesh Padakanti <padakanti.mahesh@gmail.com>
2026-08-15 23:36:46 +0400

    Delete run_app.bat

--- run_app.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-py -m streamlit run app.py
-if errorlevel 1 (
-  echo.
-  echo The application could not start. Install dependencies first with:
-  echo py -m pip install -r requirements.txt
-  pause
-)
